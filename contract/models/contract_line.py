3b045f935895b9ada852a14e97161e915af6bbf8
carlos vizcaya <commit@ingeint.com>
2022-09-13 09:21:27 -0400

    Remove validations in line

--- contract/models/contract_line.py
+++ contract/models/contract_line.py
@@ -530,7 +530,7 @@ class ContractLine(models.Model):
                 and rec.date_start
                 and today >= rec.date_start
             ):
-                rec.create_invoice_visibility = bool(rec.recurring_next_date)
+                rec.create_invoice_visibility
             else:
                 rec.create_invoice_visibility = False
 
