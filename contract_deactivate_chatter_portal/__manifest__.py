1b947afb3b47b5bcc0ca633be87b4331e256b0dd
yorlysoropeza <yorlys.oropeza@ingeint.com>
2023-05-08 15:09:47 -0400

    add new module for deativate chatter in contracts portal

---
+++ contract_deactivate_chatter_portal/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Disabled Communication Section in Portal Contracts",
+    "version": "14.0.2.3.4",
+    "category": "Contract Management",
+    "license": "AGPL-3",
+    "author": "INGEINT C.A",
+    "website": "https://ingeint.com",
+    "depends": ["contract"],
+    "data": [
+        "views/contract_portal_templates.xml",
+    ],
+    "installable": True,
+}
